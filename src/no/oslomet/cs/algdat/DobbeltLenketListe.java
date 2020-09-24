6f4c3b53ed8a57c891638c33d50b4ee66ef91c70
KhalidAFarah <56063705+KhalidAFarah@users.noreply.github.com>
2020-09-24 10:58:56 +0200

    ferdig med oppgave 2b
    kjørte testene men også dem oppgitt på pdf

--- src/no/oslomet/cs/algdat/DobbeltLenketListe.java
+++ src/no/oslomet/cs/algdat/DobbeltLenketListe.java
@@ -20,13 +20,42 @@ public class DobbeltLenketListe<T> implements Liste<T> {
 
     public static void main(String[] args){
         String[] strings = {"kd","pe", "dt"};
-        DobbeltLenketListe<String> liste = new DobbeltLenketListe<>();
+        DobbeltLenketListe<String> liste = new DobbeltLenketListe<>(strings);
         System.out.println(liste.antall() + " " + liste.tom());
         // Utskrift: 3 false
 
 
         System.out.println(liste);
         System.out.println(liste.omvendtString());
+
+        String[] s1 = {}, s2 = {"A"}, s3 = {null,"A",null,"B",null};
+        DobbeltLenketListe<String> l1 = new DobbeltLenketListe<>(s1);
+        DobbeltLenketListe<String> l2 = new DobbeltLenketListe<>(s2);
+        DobbeltLenketListe<String> l3 = new DobbeltLenketListe<>(s3);
+        System.out.println(l1.toString() + " " + l2.toString()
+                + " " + l3.toString() + " " + l1.omvendtString() + " "
+                + l2.omvendtString() + " " + l3.omvendtString());
+        // Utskrift: [] [A] [A, B] [] [A] [B, A]
+
+        l2.leggInn("A");
+        l3.leggInn("C");
+        System.out.println(l1.toString() + " " + l2.toString()
+                + " " + l3.toString() + " " + l1.omvendtString() + " "
+                + l2.omvendtString() + " " + l3.omvendtString());
+
+        DobbeltLenketListe<Integer> liste5 = new DobbeltLenketListe<>();
+        System.out.println(liste5.toString() + " " + liste5.omvendtString());
+        for (int i = 1; i <= 3; i++)
+        {
+            liste5.leggInn(i);
+            System.out.println(liste5.toString() + " " + liste5.omvendtString());
+        }
+        // Utskrift:
+        // [] []
+        // [1] [1]
+        // [1, 2] [2, 1]
+        // [1, 2, 3] [3, 2, 1]
+
     }
 
     /**
@@ -102,7 +131,24 @@ public class DobbeltLenketListe<T> implements Liste<T> {
 
     @Override
     public boolean leggInn(T verdi) {
-        throw new UnsupportedOperationException();
+        Objects.requireNonNull(verdi);
+
+        if (hode == null && hale == null && antall == 0) {
+            hode = new Node<T>(verdi, null, null);
+            hale = hode;
+            antall++;
+            endringer++;
+            return true;
+        } else if (antall > 0 && hale != null) {
+            Node<T> newNode = new Node<>(verdi, hale, null);
+            hale.neste = newNode;
+            hale = newNode;
+            antall++;
+            endringer++;
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
